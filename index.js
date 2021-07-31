ff90be029f90ee9fe38a3bc84285a6db8fdb99ea
Nupur Khatri <nupurkhatri.ict19@gmail.com>
2021-07-31 19:09:53 +0530

    Update Sliders js

---
+++ index.js
@@ -0,0 +1,30 @@
+var F=true;
+var S=false;
+var A=false;
+
+function Fa() {
+    F=true;
+    S=false;
+    A=false;
+}
+function Sd() {
+    F=true;
+    S=false;
+    A=false;
+}
+function Ar() {
+    F=true;
+    S=false;
+    A=false;
+}
+
+if(F == true) {
+    document.getElementsByClassName("slider").setAttribute("src","DeepSiteBackground.jpg"); 
+}
+
+if(S == true) {
+    document.getElementsByClassName("slider").setAttribute("src","atr.jpg"); 
+}
+if(A == true) {
+    document.getElementsByClassName("slider").setAttribute("src","img1.jpg"); 
+}
