5c2f09dc952a74681cbaf82ae72704f7a64120a1
Nupur Khatri <nupurkhatri.ict19@gmail.com>
2021-08-09 19:52:31 +0530

    Update

--- js/index.js
+++ js/index.js
@@ -5,22 +5,26 @@ function function1() {
     $(window).scroll(function () {
         window.scrollBy({ behavior: 'smooth' });
         scroll_pos = $(window).scrollTop();
-        // if(scroll_pos >= 1400 && scroll_pos <= 2028){
-        //     $("nav").css("background-image","linear-gradient(to right,rgba(241, 232, 230, 0),rgba(72, 77, 73,0))");
-        //     // $("nav ul li a").css("color","black");
-        //     $(".logo a img").css('max-width', '100px');
-        //     $("nav").css("transition","2s");
-        // }
-        // else 
-        if (scroll_pos >= 100) {
-            $("nav").css("background-image","linear-gradient(to right,rgba(241, 232, 230, 0.40),rgba(72, 77, 73,0.60))"); 
-            $(".logo a img").css('max-width', '100px');
-            // $("nav ul li a").css("color","white");
+        if(scroll_pos >= 986 && scroll_pos <= 1843){
+            $("nav").css("background-image","linear-gradient(to right,rgba(255, 255, 251, 0.80),rgba(255, 255, 251, 0.80))"); 
+            $(".logo a img").css('max-width', '80px');
+            $("nav ul").css("margin-top","0.2vh");
+            $("nav ul li a").css("color","black");
+            $(".img1").css("animation","img11 3s ease-in-out 0s 1");
+        }
+        else if (scroll_pos >= 100) {
+            $("nav").css("background-image","linear-gradient(to right,rgba(255, 255, 251, 0.80),rgba(255, 255, 251, 0.80))"); 
+            $(".logo a img").css('max-width', '80px');
+            $("nav ul").css("margin-top","0.2vh");
+            $("nav ul li a").css("color","black");
+            $(".img1").css("animation","none");
         }
         else {
             $("nav").css("background-image","linear-gradient(to right,rgba(72, 77, 73,0),rgba(241, 232, 230,0),rgba(72, 77, 73,0))"); 
             $(".logo a img").css('max-width', '160px');
-            // $("nav ul li a").css("color","white");
+            $("nav ul").css("margin-top","1vh");
+            $("nav ul li a").css("color","white");
+            $(".img1").css("animation","none");
         }
 })
 };
