e67e353e4a82db050f7a07d5cf4e6644c2546da4
Nupur Khatri <nupurkhatri.ict19@gmail.com>
2021-08-09 20:12:57 +0530

    Update

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ function function1() {
             $("nav ul").css("margin-top","0.2vh");
             $("nav ul li a").css("color","black");
             $(".img1").css("animation","img11 3s ease-in-out 0s 1");
+           
         }
         else if (scroll_pos >= 100) {
             $("nav").css("background-image","linear-gradient(to right,rgba(255, 255, 251, 0.80),rgba(255, 255, 251, 0.80))"); 
@@ -18,6 +19,7 @@ function function1() {
             $("nav ul").css("margin-top","0.2vh");
             $("nav ul li a").css("color","black");
             $(".img1").css("animation","none");
+            
         }
         else {
             $("nav").css("background-image","linear-gradient(to right,rgba(72, 77, 73,0),rgba(241, 232, 230,0),rgba(72, 77, 73,0))"); 
@@ -25,6 +27,7 @@ function function1() {
             $("nav ul").css("margin-top","1vh");
             $("nav ul li a").css("color","white");
             $(".img1").css("animation","none");
+            
         }
 })
 };
