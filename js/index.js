541d820b7013d02b7bab0f4ca530f2443798bab1
Nupur Khatri <nupurkhatri.ict19@gmail.com>
2021-08-12 17:31:34 +0530

    update

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function function1() {
         window.scrollBy({ behavior: 'smooth' });
         scroll_pos = $(window).scrollTop();
         if (scroll_pos >= 1294 && scroll_pos <= 1894) {
-            $("nav").css("background-image","linear-gradient(to right,rgba(252, 252, 245, 0.95),rgba(252, 252, 245, 0.95))");
+            $("nav").css("background-image","linear-gradient(to right,rgba(252, 252, 245, 0.80),rgba(252, 252, 245, 0.80))");
             $(".logo a img").css('max-width', '80px');
             $("nav ul").css("margin-top","0.2vh");
             $("nav ul li a").css("color","black");
@@ -14,7 +14,7 @@ function function1() {
             $(".worktext2 h1").css("animation","typing 8s steps(34, end) 1s 1 normal both, blink 900ms steps(34) infinite normal");
         }
         else if(scroll_pos >= 993 && scroll_pos <= 1484){
-            $("nav").css("background-image","linear-gradient(to right,rgba(252, 252, 245, 0.95),rgba(252, 252, 245, 0.95))");
+            $("nav").css("background-image","linear-gradient(to right,rgba(252, 252, 245, 0.80),rgba(252, 252, 245, 0.80))");
             $(".logo a img").css('max-width', '80px');
             $("nav ul").css("margin-top","0.2vh");
             $("nav ul li a").css("color","black");
@@ -22,7 +22,7 @@ function function1() {
             $(".worktext2 h1").css("animation","none");
         }
         else if (scroll_pos >= 100) {
-            $("nav").css("background-image","linear-gradient(to right,rgba(252, 252, 245, 0.95),rgba(252, 252, 245, 0.95))"); 
+            $("nav").css("background-image","linear-gradient(to right,rgba(252, 252, 245, 0.80),rgba(252, 252, 245, 0.80))"); 
             $(".logo a img").css('max-width', '80px');
             $("nav ul").css("margin-top","0.2vh");
             $("nav ul li a").css("color","black");
