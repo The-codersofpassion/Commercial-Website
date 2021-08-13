6f0b9b719b9719546a1fdce72c998e0366fb2880
Nupur Khatri <nupurkhatri.ict19@gmail.com>
2021-08-13 18:24:44 +0530

    Update

--- js/index.js
+++ js/index.js
@@ -75,3 +75,24 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
+
+// for PSB Section
+var slideIndex1 = 0;
+showSlides1();
+
+function showSlides1() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides1");
+  var dots = document.getElementsByClassName("dot1");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex1++;
+  if (slideIndex1 > slides.length) {slideIndex1 = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex1-1].style.display = "block";  
+  dots[slideIndex1-1].className += " active";
+  setTimeout(showSlides1, 4000); // Change image every 2 seconds
+}
