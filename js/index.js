17c8f1741ad1f55d5b70d67f55ee6c92eabcf619
Nupur Khatri <nupurkhatri.ict19@gmail.com>
2021-08-13 17:14:05 +0530

    update

--- js/index.js
+++ js/index.js
@@ -56,9 +56,22 @@ function scrollto(div) {
 };
 
 
-// var typed = new Typed(".workchangetext", {
-//     strings: ["Collobarations", "Decoration", "Efficiency"],
-//     typeSpeed: 100,
-//     backSpeed: 60,
-//     loop: true
-// });
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
