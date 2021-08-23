4f98ce61cc97c8d245751e99ed6bb0acd226264f
Nupur Khatri <nupurkhatri.ict19@gmail.com>
2021-08-23 13:45:51 +0530

    edit

--- js/Fashion.js
+++ js/Fashion.js
@@ -1,3 +1,40 @@
+document.addEventListener("scroll", function1());
+
+function scrollto(div) {
+    if (div) {
+
+        $('html,body').animate(
+            {
+                scrollTop: $("#" + div).offset().top
+            });
+    }
+};
+
+function function1() {
+  var scroll_pos = 0;
+  $(window).scroll(function () {
+      window.scrollBy({ behavior: 'smooth' });
+      scroll_pos = $(window).scrollTop();
+      if (scroll_pos >= 100) {
+        $("nav").css("background-image","linear-gradient(to right,rgba(252, 252, 245, 0.80),rgba(252, 252, 245, 0.80))"); 
+        $(".logo a img").css('max-width', '80px');
+        $("nav ul").css("margin-top","0.2vh");
+        $("nav ul li a").css("color","black");
+        $("nav").css("box-shadow"," 0px 0px 10px #0000001c");
+    }
+    else {
+        $("nav").css("background-image","linear-gradient(to right,rgb(252, 252, 249,0),rgba(231, 231, 219,0))"); 
+        $(".logo a img").css('max-width', '160px');
+        $("nav ul").css("margin-top","1vh");
+        $("nav ul li a").css("color","white");
+        $("nav").css("box-shadow"," none");
+    }
+    if(scroll_pos>3601){
+      window.scrollTo(0,3601);
+    }
+})
+};
+
 var slideIndex = 0;
 showSlides();
 
