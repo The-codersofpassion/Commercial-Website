9ca9c26bb0a40c35f0f85afffba7ccc85905456a
Nupur Khatri <nupurkhatri.ict19@gmail.com>
2021-08-29 15:17:55 +0530

    update

--- js/index.js
+++ js/index.js
@@ -75,21 +75,4 @@ function showSlides() {
   setTimeout(showSlides, 8000); // Change image every 4 seconds
 }
 
-const form = document.getElementById('contact_form');
 
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        // this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('service_fq3c3h9', 'template_ras0je9', this)
-            .then(function() {
-                alert("Message successfully sent!");
-                console.log('SUCCESS!');
-                form['user_name'].reset();
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    });
-}
