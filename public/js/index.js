91175da6112b7721c8117ed34d5ed93534fe7540
Nupur Khatri <nupurkhatri.ict19@gmail.com>
2021-09-04 18:26:41 +0530

    Slider done

--- public/js/index.js
+++ public/js/index.js
@@ -75,3 +75,106 @@ function showSlides() {
   setTimeout(showSlides, 8000); // Change image every 4 seconds
 }
 
+let i = 0;
+
+        let box1 = document.getElementById("image1");
+        let box2 = document.getElementById("image2");
+        let box3 = document.getElementById("image3");
+        let a1 = document.querySelector(".a1");
+        let a2 = document.querySelector(".a2");
+        let a3 = document.querySelector(".a3");
+        let info = document.getElementsByClassName("info");
+
+        a1.onclick = function () {
+            box1.style.opacity = 1;
+            box2.style.opacity = 0;
+            box3.style.opacity = 0;
+            $('#image1').css("transform","unset");
+            $('#image2').css("transform","unset");
+            $('#image3').css("transform","unset");
+            info.innerHTML = "Image 1"
+            i = 0;
+        }
+
+        a2.onclick = function () {
+            box1.style.opacity = 0;
+            box2.style.opacity = 1;
+            box3.style.opacity = 0;
+            $('#image1').css("transform","unset");
+            $('#image2').css("transform","translateX(-100vw)");
+            $('#image3').css("transform","unset");
+            info.innerHTML = "Image 2"
+            i = 1;
+        }
+
+        a3.onclick = function () {
+            box1.style.opacity = 0;
+            box2.style.opacity = 0;
+            box3.style.opacity = 1;
+            $('#image1').css("transform","unset");
+            $('#image2').css("transform","unset");
+            $('#image3').css("transform","translateX(-200vw)");
+            info.innerHTML = "Image 3"
+            i = 2;
+        }
+
+        
+        document.addEventListener("keydown", (e) => {
+            if (e.keyCode == 37) {
+                right();
+            }
+        });
+
+        document.addEventListener("keydown", (e) => {
+            if (e.keyCode == 39 ) {
+                right();
+            }
+        });
+
+        function right() {
+            if (i == 0) {
+                box1.style.opacity = 1;
+                box2.style.opacity = 0;
+                box3.style.opacity = 0;
+                info.innerHTML = "Image 1"
+                i++
+            }
+            else if (i == 1) {
+                box1.style.opacity = 0;
+                box2.style.opacity = 1;
+                box3.style.opacity = 0;
+                info.innerHTML = "Image 2"
+                i++
+            }
+            else if (i == 2) {  
+                box1.style.opacity = 0;
+                box2.style.opacity = 0;
+                box3.style.opacity = 1;
+                info.innerHTML = "Image 3"
+                i = 0;
+            }
+        }
+
+        function left() {
+            if (i == 0) {
+                box1.style.opacity = 0;
+                box2.style.opacity = 0;
+                box3.style.opacity = 0;
+                info.innerHTML = "Image 1"
+                i++
+            }
+            else if (i == 1) {
+                box1.style.opacity = 0;
+                box2.style.opacity = 0;
+                box3.style.opacity = 1;
+                info.innerHTML = "Image 2"
+                i++
+            }
+            else if (i == 2) {
+                box1.style.opacity = 0;
+                box2.style.opacity = 1;
+                box3.style.opacity = 0;
+                info.innerHTML = "Image 3"
+                i = 0;
+            }
+        }
