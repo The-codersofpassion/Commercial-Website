f81791cc263b3782edd35f38ebbeea37c2ebef6a
Nupur Khatri <nupurkhatri.ict19@gmail.com>
2021-09-06 18:34:02 +0530

    update

--- public/js/Fashion.js
+++ public/js/Fashion.js
@@ -96,22 +96,3 @@ function showSlides2() {
   setTimeout(showSlides2, 4000); // Change image every 4 seconds
 }
 
-var slideIndex4 = 0;
-showSlides4();
-
-function showSlides4() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides4");
-  var dots = document.getElementsByClassName("dot4");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex4++;
-  if (slideIndex4 > slides.length) {slideIndex4 = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex4-1].style.display = "block";  
-  dots[slideIndex4-1].className += " active";
-  setTimeout(showSlides4, 4000); // Change image every 4 seconds
-}
